6f378ac3c1353012210f6163bc413fd95c291393
manabled <31279071+manabled@users.noreply.github.com>
2017-10-14 23:07:27 -0400

    Add files via upload

---
+++ 03_novel.py
@@ -0,0 +1,44 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin
+
+import sys
+import json
+
+assert sys.version_info >= (3,4), 'This script requires at least Python 3.4'
+
+with open('gulag.json') as json_data:
+	world = json.load(json_data)
+
+def get_response(response):
+	try :
+		return int(response)-1
+	except ValueError:
+		return -1
+
+def print_response(count,responses):
+	return str(count + 1)
+
+def check_quit(response):
+	response = str(response)
+	if response.lower() == 'q' or response.lower == 'quit':
+		return True
+	return False
+
+location = 'gulag_intro'
+
+game_is_running = True
+while game_is_running:
+	current = world[location]
+	print(current['description'])
+	if len(current['options']) == 0:
+		break
+	for count,option in enumerate(current['options']):
+		print('[' + print_response(count,current['options']) + '] ' + option['option'])
+	print('[q] to quit')
+	response = input('What do you do?')
+	if check_quit(response):
+		game_is_running = False
+		continue
+	response = get_response(response)
+	for count,option in enumerate(current['options']):
+		if (response == count):
+			location = option['goto']
